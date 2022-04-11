741a5a1ab1f23e740a18cef787920be927239d94
kgg75 <kgg75@mail.ru>
2022-04-11 14:34:33 +0700

    Update settings.py

--- NewsPortal/settings.py
+++ NewsPortal/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7aqit2d4xxtpmh+lrf@-g76kz#!3=&i(5_=8llrqkeyl-b39_+'
+SECRET_KEY = '***'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
